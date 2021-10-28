ca70a1669fd8b294cbbfce6b1d201c823dfa0d8d
Ryan Lingo <ryanlingo@gmail.com>
2021-10-28 14:15:44 -0400

    add multi row functionality

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const request = require('request-promise');
-const cheerio = require('cheerio');
+const request = require("request-promise");
+const cheerio = require("cheerio");
 
 async function main() {
   const result = await request.get(
-    'https://www.codingwithstefan.com/table-example',
+    "https://www.codingwithstefan.com/table-example"
   );
   const $ = cheerio.load(result);
-  $('body > table > tbody > tr').each((index, element) => {
-    const tds = $(element).find('td');
+  const scrapedRows = [];
+  $("body > table > tbody > tr").each((index, element) => {
+    if (index === 0) return true;
+    const tds = $(element).find("td");
     const company = $(tds[0]).text();
-    console.log(company);
+    const contact = $(tds[1]).text();
+    const country = $(tds[2]).text();
+    const scrapedRow = { company, contact, country };
+    scrapedRows.push(scrapedRow);
   });
+  console.log(scrapedRows);
 }
 
 main();
